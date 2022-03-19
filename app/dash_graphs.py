c3a8d31d0e3f1a5ba40699e01e9f177ad84d11a8
Mateusz Śledź <sledziu1508@gmail.com>
2022-03-19 12:26:56 +0100

    first graph done

--- app/dash_graphs.py
+++ app/dash_graphs.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as px
 from dash import dash, dcc, html, Input, Output
+import plotly.graph_objs as go
 
 
 app = dash.Dash(__name__)
@@ -9,12 +10,10 @@ df_hulu = pd.read_csv('../data/hulu_titles.csv')
 df_netf = pd.read_csv('../data/netflix_titles.csv')
 df_3 = pd.read_csv('../data/MoviesOnStreamingPlatforms.csv')
 
-df_hulu['dataset'] = 'Hulu'
-df_netf['dataset'] = 'Netflix'
+# df_hulu['dataset'] = 'Hulu'
+# df_netf['dataset'] = 'Netflix'
 
 df_joined = pd.concat([df_hulu, df_netf])
-
-
 g_by_release = df_joined.groupby(['release_year'])
 
 @app.callback(
@@ -22,31 +21,51 @@ g_by_release = df_joined.groupby(['release_year'])
     Input('start_y', 'value'),
     Input('end_y', 'value'))
 def update_figure(start_year, end_year):
-    filtered_df = df_joined[(start_year <= df_joined['release_year']) & (df_joined['release_year'] <= end_year)]
+    filtered_df1 = df_hulu[(start_year <= df_hulu['release_year']) & (df_hulu['release_year'] <= end_year)]
+    filtered_df2 = df_netf[(start_year <= df_netf['release_year']) & (df_netf['release_year'] <= end_year)]
 
-    g_by_release = filtered_df.groupby(['release_year'])
+    g_by_release1 = filtered_df1.groupby(['release_year'])
+    g_by_release2 = filtered_df2.groupby(['release_year'])
 
-    values_years = [count for count in g_by_release.size()]
+    values_years1 = [count for count in g_by_release1.size()]
+    values_years2 = [count for count in g_by_release2.size()]
 
-    fig = px.line(filtered_df, x=list(g_by_release.groups.keys()), y=values_years, labels={
-        'x': 'year',
-        'y': 'total count'
-    },  height=600, markers=True)
-    fig.update_layout(title_font_family='Lato')
-    #fig.update_traces(hovertemplate='year: %{label}<br>count: %{value}<extra></extra>')
+    fig = go.Figure([
+        go.Scatter(
+            name='Hulu',
+            x=list(g_by_release1.groups.keys()),
+            y=values_years1,
+            mode='markers+lines',
+            marker=dict(color='blue', size=6)
+        ),
+        go.Scatter(
+            name='Netflix',
+            x=list(g_by_release2.groups.keys()),
+            y=values_years2,
+            mode='markers+lines',
+            marker=dict(color='red', size=6)
+        )])
+
+    fig.update_layout(
+        title='Hulu and Netflix releases of movies and tv shows by year',
+        xaxis_title='Year',
+        yaxis_title='Count',
+        height=600,
+        title_font_family='Lato')
+
     fig.update_layout(transition_duration=500)
 
     return fig
 
 
+
 app.layout = html.Div(style={'fontFamily': 'Lato', 'margin': '12px 36px'}, children=[
     html.H1(children='Hulu and Netflix Movies and TV Shows'),
 
-    html.H2(children='1. Hulu movies and tv shows by year'),
     html.Div([
         html.Div([
             "Starting Year: ",
-            dcc.Dropdown(list(g_by_release.groups.keys()), 1928, id='start_y')
+            dcc.Dropdown(list(g_by_release.groups.keys()), 1923, id='start_y')
         ],
             style={
                 "width": "25%",
@@ -67,7 +86,8 @@ app.layout = html.Div(style={'fontFamily': 'Lato', 'margin': '12px 36px'}, child
         }),
     dcc.Graph(
         id='content-release-date'
-    )])
+    )
+    ])
 
 
 
